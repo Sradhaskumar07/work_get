eb2782ebc1e51ab86ecceb37d60643e4e126fa47
Sradha <sradhasunilkumar007@gmail.com>
2026-03-31 16:49:34 +0530

    Changes made for practice

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # work_get
 hands on experience with git using own repo 
 I am editing this file.
+This is a commit
--- example.py
+++ example.py
@@ -1,3 +1,4 @@
 def git_operation():
           print("I am adding example.py file to the remote repo")
+          print("This is a commit")
 git_operation()`
